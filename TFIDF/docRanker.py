ffbc8bd3d3df5073d4cfc8ae97554cba8e4bfe70
Markvarte <ladyireneadler@mail.ru>
2021-11-13 21:28:53 +0200

    Rank docs in descending order.

--- TFIDF/docRanker.py
+++ TFIDF/docRanker.py
@@ -1,6 +1,6 @@
 def create_ranked_list(posting_dict: dict):
     # sort by dict values
-    posting_dict = dict(sorted(posting_dict.items(), key=lambda item: item[1]))
+    posting_dict = dict(sorted(posting_dict.items(), key=lambda item: item[1], reverse=True))
     # return ranked keys only
     return posting_dict.keys()
 
