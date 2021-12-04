bda2d5c29435f572159238fdffa286dbf3e67fdc
Markvarte <ladyireneadler@mail.ru>
2021-12-04 20:04:29 +0200

    Remove whitespace.

--- TFIDF/docRanker.py
+++ TFIDF/docRanker.py
@@ -3,5 +3,3 @@ def create_ranked_list(posting_dict: dict):
     posting_dict = dict(sorted(posting_dict.items(), key=lambda item: item[1], reverse=True))
     # return ranked keys only
     return posting_dict.keys()
-
-
